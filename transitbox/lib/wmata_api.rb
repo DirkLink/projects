17dfa8b0124fca5371e303c0cd40473d8bb1402a
Dirk Link <dlink87@MacBook-Pro.local>
2015-06-18 13:12:57 -0400

    token logic implemented

--- transitbox/lib/wmata_api.rb
+++ transitbox/lib/wmata_api.rb
@@ -2,7 +2,7 @@ require 'httparty'
 require 'json'
 
 class WMataAPI
-  # Token = File.read "./token"
+  Token = File.read "./token"
 
   include HTTParty
   base_uri 'https://api.wmata.com'
@@ -12,14 +12,16 @@ class WMataAPI
   end
 
   def stations
-    s = BWMataAPI.get("")
+    s = WMataAPI.get("https://api.wmata.com/Rail.svc/json/jStations", query: { api_key: "#{Token}" })
     if s
+
       station_array = s["stations"]["station"].map {|station| station.values_at("name","lat","long","nbEmptyDocks","nbBikes")}
       station_array.map {|station| Hash[:name,station[0],:lat,station[1],:long,station[2],:nbEmptyDocks,station[3],:nbBikes,station[4]]}
     end
   end
 
   def get_track artist, track
+
     s = WMataAPI.get("/v1/search", query: { q: "artist:#{artist} + track:#{track}", type: "track"}) 
     # query: { q: "uprising", type: "track"}
     if s
@@ -30,7 +32,7 @@ class WMataAPI
 end
 
 # require 'pry'
-# spot = WMataAPI.new
+# t = WMataAPI.new
 # binding.pry
 
 
