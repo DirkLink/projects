e998262c1e3f84cacd1d5a75ab3b31f3cf04427f
Dirk Link <dlink87@MacBook-Pro.local>
2015-06-23 15:33:56 -0400

    question index page view set up

--- woohoo_answers/app/controllers/questions_controller.rb
+++ woohoo_answers/app/controllers/questions_controller.rb
@@ -1,2 +1,17 @@
 class QuestionsController < ApplicationController
+  def index
+    @questions = Question.all
+  end
+
+   def show
+    @question = Question.find params[:id]
+  end
+
+  def create
+    question_params = params[:question]
+    @item = Item.new(
+      title: question_params[:title],
+      description: question_params[:description]
+    )
+  end
 end
--- woohoo_answers/config/routes.rb
+++ woohoo_answers/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   resources :questions do
-    resources :answers
+    resources :answers, except: [:index, :show]
   end
 end
