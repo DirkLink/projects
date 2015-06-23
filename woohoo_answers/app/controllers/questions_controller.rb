e724fdd77dbed1e20e83d14bc8b2519bffa2a389
Dirk Link <dlink87@MacBook-Pro.local>
2015-06-23 16:13:52 -0400

    questions controller working for viewing and adding questions

--- woohoo_answers/app/controllers/questions_controller.rb
+++ woohoo_answers/app/controllers/questions_controller.rb
@@ -3,15 +3,26 @@ class QuestionsController < ApplicationController
     @questions = Question.all
   end
 
-   def show
+  def show
     @question = Question.find params[:id]
+  end
+
+  def new
+    @question = Question.new
   end
 
   def create
     question_params = params[:question]
-    @item = Item.new(
+    @question = Question.new(
       title: question_params[:title],
       description: question_params[:description]
     )
+    if @question.save
+      # redirect_to i
+      redirect_to question_path(@question), notice: "Question created"
+    else
+      # redisplay form with errors
+      render :new
+    end
   end
 end
