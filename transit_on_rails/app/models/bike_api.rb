687af7fbf08e7a847d106772881bb4e9a24d95cb
Dirk Link <dlink87@MacBook-Pro.local>
2015-06-26 09:35:23 -0400

    use pg in production

--- transit_on_rails/app/models/bike_api.rb
+++ transit_on_rails/app/models/bike_api.rb
@@ -7,32 +7,6 @@ class BikeApi
   include HTTParty
   base_uri 'https://www.capitalbikeshare.com/data/stations/bikeStations.xml'
 
-  def distance_list
-      station_array = []
-      stations = BikeStation.all
-      stations.each do |s|
-        station_array.push([s.station_id,s.address,s.distance(@loc)])
-      end
-      station_array.sort_by! {|a| a[2]}
-      station_array[0..2]
-    end
-
-  def nearest_stations
-    bikes = []
-    stations = distance_list
-    stations.each do |s|
-      id = s[0]
-      distance = s[2]
-      station = BikeStation.find_by_station_id(id)
-      info = station_info(id)
-      # binding.pry
-      # bikes = station.push(Hash[:distance, distance])
-      bikes = bikes.push(Hash[:name, station.address,:nbBikes, info.first[:nbBikes], :nbEmptyDocks, info.first[:nbEmptyDocks], :distance, distance])
-    end
-    bikes = Hash[:bike,bikes]
-    bikes
-  end
-
   def self.station_info id
     s = BikeApi.get("")
     if s
